8a87bee09ced00f6a06f1a30a2bc2d11743d7982
Patricio Carvajal Catalàn <pandcarvajalc@C02GL0KWMD6N.local>
2022-07-04 18:51:19 -0400

    Se agrega swagger

--- src/app/tasks/schemas/task.schema.ts
+++ src/app/tasks/schemas/task.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { Document } from 'mongoose';
+import { Document } from 'mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 
 export type TaskDocument = Task & Document;
